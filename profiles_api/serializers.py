fa2e679210ce24d4c53bae7b298c9302476002b9
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-17 04:24:21 +0500

    Create a user profile serializer

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,7 +1,33 @@
 from rest_framework import serializers
+from profiles_api import models
 
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView"""
 
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ("id", "email", "name", "password")  # password field is optional
+        extra_kwargs = {
+            "password": {
+                "write_only": True,  # Can not use to retrieve (GET) objects.
+                "style": {"input_type": "password"},
+            }
+        }
+
+    # Overriding a default create function of serializer to create object
+    def create(self, validate_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validate_data["email"],
+            name=validate_data["name"],
+            password=validate_data["password"],
+        )
+
+        return user
