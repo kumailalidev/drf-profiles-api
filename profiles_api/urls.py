9102d2ef44a464b355a6f7e134861ecd17676068
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-17 04:36:22 +0500

    Register profile ViewSet with the URL router

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,8 +5,14 @@ from profiles_api import views
 # Creating a router
 router = DefaultRouter()
 
-# Registering a ViewSet with router
+# Registering a HelloViewSet with router
 router.register("hello-viewset", views.HelloViewSet, basename="hello-viewset")
+
+# Registering a UserProfileViewSet with router
+# NOTE: base name is not required because it is automatically configured by DRF
+#       UserProfileViewSet has a queryset model UserProfile, basename is same
+#       as queryset model.
+router.register("profile", views.UserProfileViewSet)
 
 urlpatterns = [
     path("hello-view/", views.HelloApiView.as_view()),
